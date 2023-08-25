5a150d1a99a8508856be142a69406602a261356e
Replit user <>
2023-08-25 19:38:43 +0000

    new try

--- list.c
+++ list.c
@@ -88,11 +88,7 @@ void pushBack(List * list, void * data) {
 
 void pushCurrent(List * list, void * data) {
   
-  if(list->current == list->head)
-  {
-    pushFront(list,data);
-    
-  }else if(list->head == NULL){
+  if(list->head == NULL){
     pushFront(list,data);
     
   }else if(list->current == list->tail){
